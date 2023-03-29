26fb2fa1896f2f38986d2c02bcfa14414c5c1577
LaurentDIA16 <dia16@polenumerique.re>
2023-03-29 16:00:15 +0400

    Update urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,16 +1,22 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.apiOverview, name="api-overview"),
-    path('board-list/', views.boardList, name="board-list"),
-    path('board-detail/<str:pk>/', views.boardDetail, name="board-detail"),
-    path('board-create/', views.boardCreate, name="board-create"),
-    path('board-update/<str:pk>/', views.boardUpdate, name="board-update"),
-    path('board-delete/<str:pk>/', views.boardDelete, name="board-delete"),
-    path('item-list/', views.itemList, name="item-list"),
-    path('item-detail/<str:pk>/', views.itemDetail, name="item-detail"),
-    path('item-create/', views.itemCreate, name="item-create"),
-    path('item-update/<str:pk>/', views.itemUpdate, name="item-update"),
-    path('item-delete/<str:pk>/', views.itemDelete, name="item-delete"),
 ]
+
+
+# urlpatterns = [
+#     path('', views.apiOverview, name="api-overview"),
+#     path('boards/', views.get_data, name='boards'),
+#     path('board-list/', views.boardList, name="board-list"),
+#     path('board-detail/<str:pk>/', views.boardDetail, name="board-detail"),
+#     path('board-create/', views.boardCreate, name="board-create"),
+#     path('board-update/<str:pk>/', views.boardUpdate, name="board-update"),
+#     path('board-delete/<str:pk>/', views.boardDelete, name="board-delete"),
+#     path('item-list/', views.itemList, name="item-list"),
+#     path('item-detail/<str:pk>/', views.itemDetail, name="item-detail"),
+#     path('item-create/', views.itemCreate, name="item-create"),
+#     path('item-update/<str:pk>/', views.itemUpdate, name="item-update"),
+#     path('item-delete/<str:pk>/', views.itemDelete, name="item-delete"),
+# ]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+
+from api.views import boardViewSet, itemViewSet
+
+router = DefaultRouter()
+
+router.register("boards", boardViewSet, basename="board")
+router.register("items", itemViewSet, basename="item")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include(router.urls)),
 ]
