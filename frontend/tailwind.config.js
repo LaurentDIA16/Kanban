37bf3df5bf02094465a01b04919ff86f835b972b
LaurentDIA16 <dia16@polenumerique.re>
2023-03-30 08:42:10 +0400

    Installation Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  purge: ['./templates/**/*.html', './static/**/*.css'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
