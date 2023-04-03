2c9d68fd4477cece8e37ce66832da57c9fdc183f
LaurentDIA16 <dia16@polenumerique.re>
2023-04-03 17:29:01 +0400

    Update views
    Enlever dans l'adresse templates/

--- src/frontend/views.py
+++ src/frontend/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'templates/frontend/index.html')
+    return render(request, 'frontend/index.html')
